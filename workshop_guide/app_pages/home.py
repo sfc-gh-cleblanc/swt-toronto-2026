fa3dce2ebd3e001439119d84456e1b74e6819ff2
Craig LeBlanc <craig.leblanc@snowflake.com>
2026-09-14 09:00:20 -0400

    Increase SWT image width 15% to 172px

--- workshop_guide/app_pages/home.py
+++ workshop_guide/app_pages/home.py
@@ -5,7 +5,7 @@ _STATIC = Path(__file__).parent.parent / "static"
 
 col_logo, col_leaf = st.columns([1, 1])
 with col_logo:
-    st.image(str(_STATIC / "snowflake_world_tour.png"), width=150)
+    st.image(str(_STATIC / "snowflake_world_tour.png"), width=172)
 with col_leaf:
     st.image(str(_STATIC / "maple_leaf.jpg"), width=150)
 
